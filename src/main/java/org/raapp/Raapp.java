daa66ad8659cfd234b2a033399eb118cb0c24fe3
Tobias Glassner <tobias.glassner@westernacher.com>
2017-03-27 15:30:04 +0200

    Initial commit

---
+++ src/main/java/org/raapp/Raapp.java
@@ -0,0 +1,12 @@
+package org.raapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Raapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Raapp.class, args);
+	}
+}
